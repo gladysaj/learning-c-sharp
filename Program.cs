a08a054beb59a1c6ceb6ae1407f51e891066a3f2
gladyscodes <glad.alvarez1@gmail.com>
2020-12-08 22:55:55 -0600

    run c# in vscode

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Run C# in Vscode: go to view-->terminal then add: 
+// dotnet new console --force
+// dotnet build
+// dotnet run 
+
+using System;
+
+namespace learning_c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Gladys!");
+        }
+    }
+}
