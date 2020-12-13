2ac142fd1bfd5a8c738441e8e2add403a506349b
gladyscodes <glad.alvarez1@gmail.com>
2020-12-12 23:02:08 -0600

    variables

--- Program.cs
+++ Program.cs
@@ -11,7 +11,38 @@ namespace learning_c_sharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Gladys!");
+        Console.WriteLine("Hello World!"); // First hello world 
+            // Prints a message to the same line 
+            Console.Write("Congratulations");
+            Console.Write(" ");
+            Console.Write("You wrote your first lines of code");
+            // Create a char literal 
+            Console.WriteLine('b');
+            // Int literal
+            Console.WriteLine(123);
+            // Decimal literal, the m is called a literal suffix (tells the compiler is  a decimal value)
+            Console.WriteLine(12.3m);
+            // Bool literal
+            Console.WriteLine(true);
+            Console.WriteLine(false);
+            // Declaring a variable
+            string firstName;
+            // Assigning a variable 
+            firstName = "Gladys";
+            // Retrieve a value stored in a variable 
+            Console.WriteLine(firstName);
+            // Reassign the value of a variable 
+            firstName = "Beth";
+            // Initializing the variable 
+            string value = "Bob";
+            // Implicitly typed local variables (instructs the C# compiler to infer the type)
+            var message = "Hello world!";
+            // Challenge 
+            string name = "Bob";
+            int inbox = 3;
+            decimal temperature = 34.4m;
+            Console.WriteLine($"Hello, {name}! You have {inbox} in your inbox. The temperature is {temperature} celsius.");
+    
         }
     }
 }
