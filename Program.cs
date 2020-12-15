1e3b4be2d726cf604dbdf3ec54fef8f3d15e8e98
gladyscodes <glad.alvarez1@gmail.com>
2020-12-14 22:53:17 -0600

    add decision logic

--- Program.cs
+++ Program.cs
@@ -117,6 +117,221 @@ namespace learning_c_sharp
            int fahrenheit = 94;
            decimal convertToCelsius = (decimal)(fahrenheit - 32) * 5 / 9;
            Console.Write($"The temperature is {convertToCelsius} Celsius.");
+
+           // Generate random number 
+           Random dice = new Random();
+           int roll = dice.Next(1, 7);
+           Console.WriteLine(roll);
+
+           // Creating an instance of a class (is called an object)
+           Random dice = new Random();
+
+           // Overloaded method 
+           //For example, the Console.WriteLine() method has 19 different overloaded versions.
+           // The Random.Next() method has three overloaded versions
+           Random dice = new Random();
+           int roll1 = dice.Next();
+           int roll2 = dice.Next(101);
+           int roll3 = dice.Next(50, 101);
+
+           Console.WriteLine($"First roll: {roll1}");
+           Console.WriteLine($"Second roll: {roll2}");
+           Console.WriteLine($"Third roll: {roll3}");
+
+           // Challenge: find the larger of two numbers
+          int firstValue = 800;
+          int secondValue = 600;
+          int largerValue = Math.Max(firstValue, secondValue);
+          Console.WriteLine($"The larger value is: {largerValue}");
+
+          // Adding decisions
+          Random dice = new Random();
+
+          int roll1 = dice.Next(1,7);
+          int roll2 = dice.Next(1,7);
+          int roll3 = dice.Next(1,7);
+
+          int total = roll1 + roll2 + roll3;
+
+          Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+          if (total > 14)
+          {
+             Console.WriteLine("You win!!!");
+          }
+          if (total < 15 )
+          {
+             Console.WriteLine("You lose!!!");
+          }
+
+          // Boolean expressions
+          string message = "The quick brown fox jumps over the lazy dog.";
+          bool result = message.Contains("dog");
+          Console.WriteLine(result);
+
+          if (message.Contains("fox"))
+          {
+              Console.WriteLine("What does the fox say?");
+          }
+
+          // Doubles and triples example
+          Random dice = new Random();
+
+          int roll1 = dice.Next(1, 7);
+          int roll2 = dice.Next(1, 7);
+          int roll3 = dice.Next(1, 7);
+
+          int total = roll1 + roll2 + roll3;
+
+          Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+          if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+          {
+             Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+             total += 2;
+          }
+
+          if ((roll1 == roll2) && (roll2 == roll3)) 
+          {
+             Console.WriteLine("You rolled triples!  +6 bonus to total!");
+             total += 6;
+          }
+
+          if (total >= 15)
+          {
+             Console.WriteLine("You win!");
+          }
+
+         if (total < 15)
+         {
+            Console.WriteLine("Sorry, you lose.");
+         }
+
+         // Using else instead
+         Random dice = new Random();
+
+         int roll1 = dice.Next(1, 7);
+         int roll2 = dice.Next(1, 7);
+         int roll3 = dice.Next(1, 7);
+
+         int total = roll1 + roll2 + roll3;
+
+         Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+         if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+         {
+         Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+         total += 2;
+         }
+
+         if ((roll1 == roll2) && (roll2 == roll3))
+        {
+           Console.WriteLine("You rolled triples!  +6 bonus to total!");
+           total += 6;
+        }
+
+        if (total >= 15)
+        {
+           Console.WriteLine("You win!");
+        }
+        else 
+        {
+          Console.WriteLine("Sorry, you lose.");
+        }
+        
+        // Nesting 
+        Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+        if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+        {
+        if ((roll1 == roll2) && (roll2 == roll3))
+        {
+            Console.WriteLine("You rolled triples!  +6 bonus to total!");
+            total += 6;
+        }
+        else
+        {
+            Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+            total += 2;
+        }
+        }
+
+        if (total >= 15)
+        {
+            Console.WriteLine("You win!");
+        }
+        else 
+        {
+            Console.WriteLine("Sorry, you lose.");
+        }
+      
+        // Using else if 
+        Random dice = new Random();
+
+        int roll1 = dice.Next(1, 7);
+        int roll2 = dice.Next(1, 7);
+        int roll3 = dice.Next(1, 7);
+
+        int total = roll1 + roll2 + roll3;
+
+        Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+        if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+        {
+            if ((roll1 == roll2) && (roll2 == roll3))
+        {
+            Console.WriteLine("You rolled triples!  +6 bonus to total!");
+            total += 6;
+        }
+        else
+        {
+            Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+            total += 2;
+        }
+        }
+
+        if (total >= 16)
+        {
+            Console.WriteLine("You win a new car!");
+        }
+        else if (total >= 10)
+        {
+        Console.WriteLine("You win a new laptop!");
+        }
+        else if (total == 7)
+        {
+        Console.WriteLine("You win a trip for two!");
+        }
+        else
+        {
+        Console.WriteLine("You win a kitten!");
+        }
+
+        // Challenge check subscriptions expiration
+        Random random = new Random();
+        int daysUntilExpiration = random.Next(12);
+        int discountPercentage = 0;
+
+        // Your code goes here
+        Console.WriteLine(daysUntilExpiration);
+        if (daysUntilExpiration >= 10)
+        {
+            Console.WriteLine("Your subscription will expire soon.\nRenew now!");
+        }
+
+        else if (daysUntilExpiration >= 5)
+        {
+            Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save 10%!");
+        }
+
+        else if (daysUntilExpiration == 0)
+        {
+            Console.WriteLine("Your subscription has expired.");
+        }
+        if (discountPercentage > 0)
+        {
+            Console.WriteLine($"Renew now and save {discountPercentage}%.");
+        }
         }
     }
 }
