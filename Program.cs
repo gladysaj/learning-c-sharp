b90de8028f37118ad418fa0a4985318d32c793ee
gladyscodes <glad.alvarez1@gmail.com>
2020-12-14 23:50:43 -0600

    arrays store and iterate

--- Program.cs
+++ Program.cs
@@ -332,6 +332,85 @@ namespace learning_c_sharp
         {
             Console.WriteLine($"Renew now and save {discountPercentage}%.");
         }
+
+        // Declare a new array
+        string[] fraudulentOrderIDs = new string[3];
+
+        // Assign values to an element on an array
+        string[] fraudulentOrderIDs = new string[3];
+
+        fraudulentOrderIDs[0] = "A123";
+        fraudulentOrderIDs[1] = "B456";
+        fraudulentOrderIDs[2] = "C789";
+        // fraudulentOrderIDs[3] = "D000"; ( this one caused an exception larger than the array)
+
+        // Retrieve values of an array 
+        Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+        Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+        Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+        // Reassign the value of an array 
+        fraudulentOrderIDs[0] = "F000";
+        Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+        // Initialize an array 
+        string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
+
+        // Getting the size of an array 
+        Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+
+        // Looping through an array with foreach
+        string[] names = { "Bob", "Conrad", "Grant" };
+        foreach (string name in names)
+        {
+            Console.WriteLine(name);
         }
+
+        // Initialize an array of int 
+        int[] inventory = { 200, 400, 750, 175, 250 };
+
+        // Iterate with foreach
+        int[] inventory = { 200, 400, 750, 175, 250 };
+
+        foreach (int items in inventory) 
+        {
+            Console.WriteLine(items);
+        }
+
+        // Add a variable to sum the value of each element in the array
+        int[] inventory = { 200, 400, 750, 175, 250 };
+        int sum = 0;
+        foreach (int items in inventory) 
+        {
+            sum += items;
+            Console.WriteLine(items);
+        }
+
+        // Display the final value of sum 
+        Console.WriteLine($"We have {sum} items in the inventory");
+        }
+
+        // Option to display the running total
+        int[] inventory = { 200, 450, 700, 175, 250 };
+        int sum = 0;
+        int bin = 0;
+        foreach (int items in inventory)
+        {
+            sum += items;
+            bin++;
+            Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+        }
+        Console.WriteLine($"We have {sum} items in inventory.");
+
+        // Challenge - find in an array an element starting with letter B
+        string[] orders = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+        foreach (string order in orders)
+        {
+        if (order.StartsWith("B")) 
+        {
+        Console.WriteLine($"Order: {order} starts with letter B");
+        }
+    }
     }
 }
